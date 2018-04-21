f017a7bea04aae14315fc9ee61e3f92f77245ed4
Andrew.Grant <adgrant93@gmail.com>
2018-04-21 14:06:10 -0400

    Finished displaySearch.java. Just need to finish DisplaySearchAdapter and it will be ready for testing.

--- app/src/main/java/com/jskel/android/searchit/displaySearch.java
+++ app/src/main/java/com/jskel/android/searchit/displaySearch.java
@@ -17,7 +17,7 @@ public class displaySearch extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mlayoutManager;
+    private RecyclerView.LayoutManager mLayoutManager;
     private List<Results> resultsList = new ArrayList<>();
 
     @Override
@@ -25,9 +25,13 @@ public class displaySearch extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_search);
         mRecyclerView = (RecyclerView) findViewById(R.id.searchResultRecyclerView);
-        mRecyclerView.setHasFixedSize(true);
-        mlayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mlayoutManager);
+        mRecyclerView.setHasFixedSize(true); //For efficiency since all views in list will be same size
+        mLayoutManager = new LinearLayoutManager(this); //Determines how the layout will be displayed (Linear)
+        mRecyclerView.setLayoutManager(mLayoutManager); //Sets the newly created 
+        Results results = new Results("Hello Bob", "I said HOW ARE YOU TODAY BOB!");
+        resultsList.add(results);
+
+
 
     }
 }
@@ -53,4 +57,4 @@ public class displaySearch extends AppCompatActivity {
         searchArray.add("Hola!");
         ListView listView = (ListView) findViewById(R.id.searchList);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, searchArray);
-        listView.setAdapter(adapter);*/ 
+        listView.setAdapter(adapter);*/
