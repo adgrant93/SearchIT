1235f7a1ea390eb97b858e100a2f66968f6f0ca4
Andrew.Grant <adgrant93@gmail.com>
2018-04-21 15:49:17 -0400

    Added to the DisplaySearchAdapter class.

--- app/src/main/java/com/jskel/android/searchit/DisplaySearchAdapter.java
+++ app/src/main/java/com/jskel/android/searchit/DisplaySearchAdapter.java
@@ -1,8 +1,29 @@
 package com.jskel.android.searchit;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DisplaySearchAdapter extends RecyclerView.Adapter<DisplaySearchAdapter.ViewHolder> {
     private String[] mDataSet;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder { //my ViewHolder
+        public TextView mTextView;
+        public ViewHolder(TextView view){
+            super(view); //ViewHolder has no default constructor. Therefore it must be instantiated with a View
+            mTextView = view;
+        }
+    }
+
+    public DisplaySearchAdapter (String[] myDataSet){
+        mDataSet = myDataSet;
+    }
+
+    @Override
+    public DisplaySearchAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+
+    }
     
 }
